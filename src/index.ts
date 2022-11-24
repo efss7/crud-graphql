25392d228421f7dabb97eccab08c7b6914a1e0f3
efss7 <ericfelipess07@gmail.com>
2022-11-23 22:39:10 -0300

    feat(GraphQL): Configurando ambiente de desenvolvimento GraphQL

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import path from "path";
+import { buildSchema } from "type-graphql";
+import {ApolloServer} from "apollo-server";
+import { ClientResolver } from "./ClientResolver";
+
+async function main() {
+    const schema = await buildSchema({
+        resolvers: [ClientResolver],
+        emitSchemaFile: path.resolve(__dirname, "scheme.ggl")
+    })
+
+    const server = new ApolloServer({
+        schema, 
+    })
+
+    const { url } = await server.listen()
+    console.log("Server running on" + url)
+
+}
+main()
